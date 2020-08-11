af81057bc5662a5b55b38a7ef36780d67216e90f
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-11 01:34:06 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,17 +2,13 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        :name => "Lady Montague", :age => "54"
         },
       :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :statue => "alive"
+        :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    }, 
